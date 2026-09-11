6fb608697cb72ace5adf859d73149d6781f123d2
terungwater <terungwad34@gmail.com>
2026-09-11 22:26:51 +0100

    update

--- word_modi.py
+++ word_modi.py
@@ -1,21 +1,23 @@
 
 import token_1
 
-modi_text = []
+modif_text = []
 def word_modi():
-    text = "he (up) is above any other GOD (low)"
+    text = "he (up) is above any other GOD (cap)"
     nw_text = token_1.split(text)
 
     x= 0 
     while x < len(nw_text):
         if x+1 < len(nw_text) and nw_text[x+1] == "(up)":
-            modi_text.append(str.upper(nw_text[x]))
+            modif_text.append(str.upper(nw_text[x]))
             x+= 2
         elif x+1 < len(nw_text) and nw_text[x+1]== "(low)":
-            modi_text.append(str.lower(nw_text[x]))
+            modif_text.append(str.lower(nw_text[x]))
             x+=2
+        elif x+1 < len(nw_text) and nw_text[x+1] == "(cap)":
+            modif_text.append(str.capitalize(nw_text[x]))
+            x+=2 
         else:
-            modi_text.append(nw_text[x])
+            modif_text.append(nw_text[x])
             x+=1
-word_modi()
-print(modi_text)
+    return modif_text
